c5a1dfe7d7464e3315a21bd242f64bf23fb2b064
Jadan morrow <jadanmorrow@outlook.com>
2020-08-07 11:54:43 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            SayHi("Mogley", 8);
+            SayHi("Ziggy", 6);
+            SayHi("Dela", 4);
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + ", you are " + age);
+
+        }
+
+    }
+
+}
